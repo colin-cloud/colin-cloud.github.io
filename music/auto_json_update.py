702d01e31ec58a8af9351c4ffe21332448bf0869
colin-cloud <2422706963@qq.com>
2025-10-08 22:27:36 +0800

    Site updated: 2025-10-08 22:27:19

--- music/auto_json_update.py
+++ music/auto_json_update.py
@@ -2,6 +2,7 @@ import os
 import re
 import subprocess
 import shutil
+import sys
 
 
 file_head = "title:\ndate:\ntags:\ncategories:\nkeywords:\ndescription:\ntop_img:\ncomments:\ncover:\ntoc:\ntoc_number:\ncopyright:\nmathjax:\nkatex:\nhide:\naplayer: true\n"
@@ -23,7 +24,7 @@ tab_2 = '            '
 
 music_info_list = []
 
-
+# 找文件
 def find_files(dir, extensions):
     """
     查找指定目录及其子目录中的指定文件
@@ -61,45 +62,128 @@ lrc_extensions = {
     '.lrc'
 }
 
+# 错误命名音乐列表
+error_title_music_list = []
+
+# 没有音乐源的音乐列表
+not_exist_source = []
+# 没有封面的音乐列表
+not_exist_pic = []
+# 没有歌词的音乐列表
+not_exist_lrc = []
+
+# 源不规范命名
+not_specify_source = []
+# 图片不规范命名
+not_specify_pic = []
+# 歌词不规范命名
+not_specify_lrc = []
+
+# 音乐列表
+music_info_list = None
+
+# 判断空
+def check_null(str):
+    return str is None or str == ''
+
+# 获取音乐信息 
+# music = {
+#    "title": music_name,
+#    "author": music_author,
+#    "url": music_path + audio_files[0],
+#    "pic": music_path + pic_files[0],
+#    "lrc": music_path + lrc_files[0]
+#}
 def get_music_info():
     music_info_list = []
 
     music_folder = './music_info_list'
     music_path_pre = '/music/music_info_list'
     dirs = os.listdir(music_folder)
-    print(dirs)
+
+    # 判断是否按照标准命名 音乐名_作者
+    valided = True
+
     for dir in dirs:
+        
+        if not ('_' in dir):
+            valided = False
+            error_title_music_list.append(dir)
+        else: 
+            infos = dir.split('_')
+            music_name = infos[0]
+            music_author = infos[1]
+            if (check_null(music_name) or check_null(music_author)):
+                valided = False
+                error_title_music_list.append(dir)
+            else:
+                # 打印信息
+                print(infos)
+                music_path = music_path_pre + '/' + dir + '/'
 
-        infos = dir.split('_')
-        print(infos)
-        music_name = infos[0]
-        music_author = infos[1]
-        music_path = music_path_pre + '/' + dir + '/'
+                music_folder_real = music_folder + '/' + dir
+                # 查找音频文件
+                audio_files = find_files(music_folder_real, audio_extensions)
+                # 查找封面
+                pic_files = find_files(music_folder_real, image_extensions)
+                # 查找歌词
+                lrc_files = find_files(music_folder_real, lrc_extensions)
 
-        music_folder_real = music_folder + '/' + dir
-        # 查找音频文件
-        audio_files = find_files(music_folder_real, audio_extensions)
-        # 查找封面
-        pic_files = find_files(music_folder_real, image_extensions)
-        # 查找歌词
-        lrc_files = find_files(music_folder_real, lrc_extensions)
-        music = {
-            "title": music_name,
-            "author": music_author,
-            "url": music_path + audio_files[0],
-            "pic": music_path + pic_files[0],
-            "lrc": music_path + lrc_files[0]
-        }
-        music_info_list.append(music)
-    return music_info_list
+                # 存在判断 & 规范判断
+                if (len(audio_files) == 0):
+                    global not_exist_source
+                    not_exist_source.append(dir)
+                else:
+                    if '_' in audio_files[0]:
+                        global not_specify_source
+                        not_specify_source.append(dir)
+                if (len(pic_files) == 0):
+                    global not_exist_pic
+                    not_exist_pic.append(dir)
+                else:
+                    if '_' in pic_files[0]:
+                        global not_specify_pic
+                        not_specify_pic.append(dir)
+                if (len(lrc_files) == 0):
+                    global not_exist_lrc
+                    not_exist_lrc.append(dir)
+                else:
+                    if '_' in lrc_files[0]:
+                        global not_specify_lrc
+                        not_specify_lrc.append(dir)
 
+                # music_path = music_path.replace('_', '\_')
+                music = {
+                    "title": music_name,
+                    "author": music_author,
+                    "url": music_path + audio_files[0],
+                    "pic": music_path + pic_files[0],
+                    "lrc": music_path + lrc_files[0]
+                }
+                music_info_list.append(music)
 
+    # 是否存在缺失文件
+    if (len(not_exist_source) != 0 or len(not_exist_pic) != 0 or len(not_exist_lrc) != 0):
+        print('没有音乐源：', not_exist_source)
+        print('没有封面：', not_exist_pic)
+        print('没有歌词：', not_exist_lrc)
+        valided = False
+
+    # 是否存在命名不规范
+    if (len(not_specify_source) != 0 or len(not_specify_pic) != 0 or len(not_specify_lrc) != 0):
+        print('音乐源命名不规范：', not_specify_source)
+        print('封面命名不规范：', not_specify_pic)
+        print('歌词命名不规范：', not_specify_lrc)
+        valided = False
+
+    if valided:
+        return music_info_list
+    else:
+        return None
+
+# 获取音乐列表json信息
 def get_music_json_str():
     res = ''
-
-    print('获取所有音乐信息 start--')
-    music_info_list = get_music_info()
-    print('获取所有音乐信息 end--')
 
     list_len = len(music_info_list)
     print('音乐数：' + str(list_len))
@@ -158,8 +242,7 @@ def copy_file_example():
     # 源文件路径
     source = "index.md"
     # 目标文件路径
-    destination = "index-copy.md"
-    
+    destination = "index-copy.md"   
     
     # 1. 基本复制（仅复制内容，不保留元数据）
     try:
@@ -168,6 +251,20 @@ def copy_file_example():
     except Exception as e:
         print(f"备份失败: {e}")
 
+# 判断规范
+def check_specify():
+    print('获取所有音乐信息 start--')
+    global music_info_list
+    music_info_list = get_music_info()
+    print('获取所有音乐信息 end--')
+
+    if music_info_list is None:
+        print('存在音乐信息命名不规范，取消执行，错误命名音乐列表信息如下：')
+        print(error_title_music_list)
+        # 正常退出程序
+        sys.exit()
+
+check_specify()
 
 copy_file_example()
 
